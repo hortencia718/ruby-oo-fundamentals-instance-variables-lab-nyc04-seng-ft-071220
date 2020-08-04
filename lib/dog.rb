f7eaec975f44a092fc97f77bf45e2efa3b0cf8b2
hortencia c <hortenciaecisneros@gmail.com>
2020-08-04 22:56:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,3 +2,15 @@ class Dog
   def name=(dog_name)
     this_dogs_name = dog_name
 end
+
+
+
+
+
+
+
+
+
+#  am creating a class "dog"
+# am defining that class to a variable "name and am setting that name witha argument/parameter of "dog_name.
+# then i am setting "this_dogs_name" to equal to "dog_name".
